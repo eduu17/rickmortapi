82fa040b63e447e12693375e4a461ba5c0dc2ffc
eduu17 <sranjel74@gmail.com>
2023-04-01 20:08:39 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { GlobalStyles } from "./GlobalStyles";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = axios.create({
+  baseURL: "https://rickandmortyapi.com/api",
+});
+
+export default class App extends Component {
+  state = {
+    characters: [],
+    loading: false,
+  };
+
+  getNumber = async () => {
+    const listId = [];
+    for (let number = 0; number <= 20; number++) {
+      listId.push(Math.floor(Math.random() * (826 - 1) + 1));
+    }
+    return listId;
+  };
+
+  loadData = async () => {
+    const array = await this.getNumber();
+    const response = await api.get(`/character/${array}`);
+    this.setState({
+      characters: response.data,
+      loading: true,
+    });
+  };
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  render() {
+    const { characters, loading } = this.state;
+    return (
+      <>
+        {loading
+          ? characters.map((item) => (
+              <box key={item.id}>
+                <img src={item.image} alt={item.name} />
+                <h1>{item.name}</h1>
+                <p>{item.status}</p>
+                <p>{item.species}</p>
+                <p>{item.type}</p>
+                <p>{item.gender}</p>
+                <p>{item.location.name}</p>
+              </box>
+            ))
+          : "loading..."}
+        <GlobalStyles />
+      </>
+    );
+  }
 }
-
-export default App;
